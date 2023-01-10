f71511f47b285f5e124b1c2071284c3450ffc363
Dari Goldman <darigoldman@gmail.com>
2023-01-11 00:12:16 +0100

    feat: created front-end prototype for maille stash

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Button from 'react-bootstrap/Button'
+import Yarn from './components/Yarn';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>maille stash</h1>
+      <ul id="categories">
+        <Button variant="primary">Lace</Button>
+        <Button variant="primary">Fingering</Button>
+        <Button variant="primary">Sport</Button>
+        <Button variant="primary">DK</Button>
+        <Button variant="primary">Worsted</Button>
+        <Button variant="primary">Aran</Button>
+        <Button variant="primary">Bulky</Button>
+      </ul>
+      <div className="yarns">
+        <Yarn />
+        <Yarn />
+        <Yarn />
+        <Yarn />
+        <Yarn />
+        <Yarn />
+        <Yarn />
+        <Yarn />
+        <Yarn />
+        <Yarn />
+        <Yarn />
+        <Yarn />
+        <Yarn />
+        <Yarn />
+        <Yarn />
+      </div>
     </div>
   );
 }
---
+++ src/components/Yarn.js
@@ -0,0 +1,13 @@
+import Card from "react-bootstrap/Card";
+
+const Yarn = ({props}) => {
+  return (
+    <Card className="yarn-card bg-white text-white">
+      <Card.Img src="IMG_5030.jpg" alt="Card Image" />
+      <Card.ImgOverlay>
+      </Card.ImgOverlay>
+    </Card>
+  )
+}
+
+export default Yarn
